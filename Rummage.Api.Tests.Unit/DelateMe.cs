4ed7e48c504673a48a42e51ef3264fbfccb678c1
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2023-02-18 04:21:48 +0500

    INFRA:Initialize Unit Test Project

---
+++ Rummage.Api.Tests.Unit/DelateMe.cs
@@ -0,0 +1,17 @@
+﻿//=================================================
+// Copyright (c) Coalition of Good-Hearted Engineers 
+// Free To Use Comfort and Pease
+//==================================================
+
+using Xunit;
+
+namespace Rummage.Api.Tests.Unit
+{
+    public class DelateMe
+    {
+        [Fact]
+
+        public void ShouldBeTrue () => Assert.True (true);
+    }
+
+}
